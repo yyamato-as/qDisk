5595ee11813542d97e82df0bc8b09d5670949aa1
yyamato-as <yyamato.as@gmail.com>
2022-05-22 17:14:57 +0900

    Minor tweaks

--- eDiskplot.py
+++ eDiskplot.py
@@ -1,18 +1,190 @@
 import seaborn as sns
 import qdisk.utils as utils
-import astropy.io.fits as fits
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
 from astropy.visualization import ImageNormalize
-from qdisk.classes import FitsImage
+from qdisk.classes import FitsImage, PVFitsImage
+from matplotlib import ticker
+import matplotlib.patheffects as pe
 import numpy as np
 import scipy.constants as sc
 
 ### color pallet
 from matplotlib import colors
-freeze = np.loadtxt('/home/yamato/Project/MAPS/script/MAPS_cmap.txt')
-freeze /= 255.0
-cpal = colors.ListedColormap(freeze, name='freeze')
 
-cmap = {"continuum": "inferno", "M0": cpal, "M1": "RdBu_r", "M8": "gist_heat"}
+freeze = np.loadtxt("./cmap_freeze.txt")
+freeze /= 255.0
+cpal = colors.ListedColormap(freeze, name="freeze")
+
+cmap = {
+    "continuum": "inferno",
+    "M0": cpal,
+    "M1": "RdBu_r",
+    "M8": "gist_heat",
+    "channel_map": cpal,
+}
+
+
+def is_within(value, range):
+    return np.logical_and(value >= range[0], value <= range[1])
+
+
+def get_figsize(ncols, nrows, max_size=()):
+    return (ncols * 3, nrows * 3)
+
+
+def get_imagegrid(npanel, pad=0.0):
+    ncols = np.ceil(npanel**0.5).astype(int)
+    nrows = np.ceil(npanel / ncols).astype(int)
+    fig = plt.figure(figsize=get_figsize(ncols, nrows))
+    imgrid = ImageGrid(
+        fig, rect=111, nrows_ncols=(nrows, ncols), share_all=True, axes_pad=pad
+    )
+    return fig, imgrid
+
+
+def left_bottom_ax(imgrid):
+    nrows, ncols = imgrid.get_geometry()
+    i = ncols * (nrows - 1)
+    return imgrid[i]
+
+
+def plot_PV_diagram(
+    fitsname,
+    ax,
+    data_scaling_factor=1,
+    xaxis="pos",
+    cmap=None,
+    colorbar=True,
+    norm_kwargs=dict(),
+    cbar_kw=dict(),
+    beam_kw=dict(),
+    sbar_kw=dict(),
+):
+
+    # read FITS
+    image = PVFitsImage(fitsname)
+
+    if image.ndim != 2:
+        raise ValueError("The data is not in 2D.")
+
+    # data
+    data = image.data * data_scaling_factor
+
+    # get coordinate
+    image.get_coord()
+
+    # normalization
+    norm = ImageNormalize(data, **norm_kwargs)
+
+    # plot
+    utils.plot_2D_map(
+        data=data,
+        X=image.p if xaxis == "pos" else image.v,
+        Y=image.v if xaxis == "pos" else image.p,
+        ax=ax,
+        cmap=True,
+        cmap_method="pcolorfast",
+        contour=False,
+        colorbar=colorbar,
+        cmap_kw=dict(cmap=cmap, norm=norm),
+        cbar_kw=cbar_kw,
+        beam_kw=beam_kw,
+        sbar_kw=sbar_kw,
+    )
+
+
+def plot_channel_map(
+    fitsname,
+    center_coord=None,
+    rmax=10.0,
+    vrange=None,
+    thin=1,
+    sigma_clip=None,
+    rms=None,
+    noisemask_kw=dict(rmin=10.0, rmax=15.0),
+    pad=0.0,
+):
+    # load the imagecube
+    print("Loading data...")
+    imagecube = FitsImage(fitsname)
+    imagecube.get_directional_coord(center_coord=center_coord)
+    imagecube.get_spectral_coord()
+
+    # measure the rms for better visualization
+    if rms is None and sigma_clip is not None:
+        print("Estimating rms...")
+        imagecube.estimate_rms(**noisemask_kw)
+        rms = imagecube.rms
+        print("rms: {:.2f} mJy/beam".format(imagecube.rms * 1e3))
+
+    # select the velocity channels to plot
+    velax = imagecube.v[is_within(imagecube.v, vrange)]
+    velax = velax[::thin]  # skip each *thin* channels to reduce the number of channels to plot
+
+    # setup imagegrid
+    fig, imgrid = get_imagegrid(velax.size, pad=pad)
+
+    # image normalization and clipping
+    data = imagecube.data[is_within(imagecube.v, vrange),:,:][::thin] # limit to relevant channels
+    if sigma_clip is not None:
+        data[data < sigma_clip * rms] = np.nan
+    norm = ImageNormalize(
+        data, vmin=sigma_clip * rms if sigma_clip is not None else 0.0
+    )
+
+    # iterate over channels to plot
+    for i, v in enumerate(velax):
+        # define ax
+        ax = imgrid[i]
+
+        # get data
+        # im = data[imagecube.v == v, :, :].squeeze()
+        im = data[i]
+
+        # plot
+        print("Plotting v = {:.2f} km/s...".format(v))
+        utils.plot_2D_map(
+            data=im,
+            X=imagecube.x,
+            Y=imagecube.y,
+            ax=ax,
+            cmap=True,
+            cmap_method="pcolorfast",
+            contour=False,
+            colorbar=False,
+            cmap_kw=dict(cmap=cmap["channel_map"], norm=norm),
+        )
+
+        # set the spatial range
+        ax.set(xlim=(rmax, -rmax), ylim=(-rmax, rmax))
+
+        # annotate the velocity value of the channel
+        ax.annotate(
+            text="{:.2f} km/s".format(v),
+            xy=(0.95, 0.95),
+            ha="right",
+            va="top",
+            xycoords="axes fraction",
+            color="black",
+            path_effects=[pe.Stroke(linewidth=3, foreground="white"), pe.Normal()],
+        )
+
+        # set ticks
+        ax.xaxis.set_major_locator(ticker.MaxNLocator(5))
+        ax.yaxis.set_major_locator(ticker.MaxNLocator(5))
+
+    ### ImageGrid should have param of *ngrids*, but specifying this param cause an error (maybe bug?).
+    ### Here is workaround for that, removing axes on which no data are drawn.
+    for i in range(i + 1, len(imgrid)):
+        imgrid[i].set_axis_off()
+
+    # axes label in the bottom left panel
+    left_bottom_ax(imgrid).set(
+        xlabel="$\Delta$R.A. [arcsec]", ylabel="$\Delta$Dec. [arcsec]"
+    )
+
+    return fig
 
 
 def plot_map(
@@ -28,7 +200,7 @@ def plot_map(
     norm_kwargs=dict(),
     cbar_kw=dict(),
     beam_kw=dict(),
-    sbar_kw=dict()
+    sbar_kw=dict(),
 ):
 
     # read FITS
@@ -66,17 +238,18 @@ def plot_map(
         cmap_kw=dict(cmap=cmap, norm=norm),
         cbar_kw=cbar_kw,
         beam_kw=beam_kw,
-        sbar_kw=sbar_kw
+        sbar_kw=sbar_kw,
     )
 
+
 def overlay_contour(
-    fitsname, 
-    ax, 
-    center_coord=None, 
+    fitsname,
+    ax,
+    center_coord=None,
     data_scaling_factor=1.0,
     levels=None,
-    colors="black"
-    ):
+    colors="black",
+):
 
     image = FitsImage(fitsname)
     data = image.data * data_scaling_factor
@@ -91,11 +264,13 @@ def overlay_contour(
         cmap=False,
         contour=True,
         colorbar=False,
-        contour_kw=dict(levels=levels, colors=colors)
+        contour_kw=dict(levels=levels, colors=colors),
     )
 
 
-def plot_radial_profile(filename, ax, color="tab:blue", normalize=False, scale=1.0, label=None):
+def plot_radial_profile(
+    filename, ax, color="tab:blue", normalize=False, scale=1.0, label=None
+):
 
     x, y, dy = np.loadtxt(filename, unpack=True)
 
@@ -107,10 +282,8 @@ def plot_radial_profile(filename, ax, color="tab:blue", normalize=False, scale=1
     dy *= scale
 
     ax.plot(x, y, color=color, label=label)
-    ax.fill_between(x, y-dy, y+dy, facecolor=color, alpha=0.25, edgecolor=None)
+    ax.fill_between(x, y - dy, y + dy, facecolor=color, alpha=0.25, edgecolor=None)
     # ax.axhline(y=0.0, color="gray", ls="dashed")
 
-    ax.set_aspect(1./ax.get_data_ratio()/sc.golden_ratio)
+    ax.set_aspect(1.0 / ax.get_data_ratio() / sc.golden_ratio)
     ax.set(xlim=(x.min(), x.max()), ylim=(-dy.mean(), None))
-
-
--- plot_channel_maps.py
+++ plot_channel_maps.py
@@ -1,5 +1,87 @@
+import numpy as np
 from qdisk.classes import FitsImage
+from astropy.coordinates import SkyCoord
 from qdisk.utils import plot_2D_map
+import matplotlib.pyplot as plt
+from eDisk_source_dict import source_dict
+from mpl_toolkits.axes_grid1 import ImageGrid, Grid
+from astropy.visualization import AsinhStretch, ImageNormalize
+from matplotlib import ticker
+from eDiskplot import cmap
+import matplotlib.patheffects as pe 
+import sys
 
+plt.rcParams.update({
+    "xtick.top": True,
+    "ytick.right": True,
+    "xtick.direction": "in",
+    "ytick.direction": "in"})
 
-imagepath
+imagepath = "/raid/work/yamato/edisk_data/L1489IRS/eDisk_image_products/"
+source = sys.argv[1]
+molecular_lines = sys.argv[2]
+baseline = sys.argv[3]
+# vrange = source_dict[source]["emission_extent"][molecular_lines]
+vrange = (5.8, 8)
+rmax = 5 #x.5 is recommended for nicer ticker
+sigma_clip = 2
+center_coord = SkyCoord(source_dict[source]["radec"], frame="icrs")
+
+# load the image cube and coordinate
+imagename = imagepath + "{:s}_{:s}_{:s}_robust_0.5.image.fits".format(
+    source, baseline, molecular_lines
+)
+imagecube = FitsImage(imagename)
+imagecube.get_directional_coord(center_coord=center_coord)
+imagecube.get_spectral_coord()
+print("Estimating rms...")
+imagecube.estimate_rms(rmin=12, rmax=15)
+print("rms: {:.2f} mJy/beam".format(imagecube.rms*1e3))
+
+# select channels to plot
+velax = imagecube.v[(imagecube.v >= vrange[0]) & (imagecube.v <= vrange[1])]
+
+# set up image grid
+ncols = np.ceil(velax.size**0.5).astype(int)
+nrows = np.ceil(velax.size / ncols).astype(int)
+# fig, axes = plt.subplots(ncols=ncols, nrows=nrows, sharex=True, sharey=True)
+fig = plt.figure(figsize=(ncols*3, nrows*3))
+imgrid = ImageGrid(fig, rect=111, nrows_ncols=(nrows, ncols), share_all=True, axes_pad=0.0)
+
+def left_bottom_ax(imgrid, nrows, ncols):
+    i = ncols * (nrows - 1)
+    return imgrid[i]
+
+for i, v in enumerate(velax):
+    ax = imgrid[i]
+    data = imagecube.data[imagecube.v == v, :, :].squeeze()
+    data[data < sigma_clip*imagecube.rms] = np.nan
+    norm = ImageNormalize(vmin=sigma_clip*imagecube.rms)
+
+    print("Plotting v = {:.2f} km/s...".format(v))
+    plot_2D_map(
+        data=data,
+        X=imagecube.x,
+        Y=imagecube.y,
+        ax=ax,
+        cmap=True,
+        cmap_method="pcolorfast",
+        contour=False,
+        colorbar=False,
+        cmap_kw=dict(cmap=cmap["channel_map"], norm=norm),
+    )
+    ax.set(xlim=(rmax, -rmax), ylim=(-rmax, rmax))
+    ax.annotate(text="{:.2f} km/s".format(v), xy=(0.95, 0.95), ha="right", va="top", xycoords="axes fraction", color="black", path_effects=[pe.Stroke(linewidth=3, foreground="white"), pe.Normal()])
+    ax.xaxis.set_major_locator(ticker.MaxNLocator(5))
+    ax.yaxis.set_major_locator(ticker.MaxNLocator(5))
+
+### ImageGrid should have param of *ngrids*, but specifying this param cause an error (maybe bug?).
+### Here is workaround for that, removing axes on which no data are drawn.
+for i in range(i+1, len(imgrid)):
+    imgrid[i].set_axis_off()
+
+left_bottom_ax(imgrid, nrows, ncols).set(xlabel="$\Delta$R.A. [arcsec]", ylabel="$\Delta$Dec. [arcsec]")
+# imgrid.set_axes_pad((0.0, 0.0))
+
+# fig.savefig("./test.pdf", dpi=300, bbox_inches="tight")
+plt.show()
