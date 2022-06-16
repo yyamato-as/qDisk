1dee72f23998a0a680781da4b59f44a3f996acab
yyamato-as <yyamato.as@gmail.com>
2022-06-16 13:12:46 +0900

    Large reformatting

--- qdisk/classes.py
+++ qdisk/classes.py
@@ -9,9 +9,10 @@ import casatools, casatasks
 from qdisk.utils import remove_casalogfile
 remove_casalogfile()
 
-deg_to_rad = np.pi / 180.0
-deg_to_arcsec = 3600.0
-rad_to_arcsec = 180.0 / np.pi * 3600.0
+deg_to_rad = np.pi / 180
+deg_to_arcsec = 3600
+rad_to_arcsec = 180 / np.pi * 3600
+arcsec_to_rad = np.pi / 3600 / 180
 ckms = ac.c.to(u.km / u.s).value
 
 axes_attr = {"Right Ascension": "x", "Declination": "y", "Stokes": "w", "Frequency": "z"}
@@ -246,7 +247,7 @@ class CasaImage:
 
 
 class FitsImage:
-    def __init__(self, fitsname):
+    def __init__(self, fitsname, squeeze=True, rel_dir_ax=True, xlim=None, ylim=None, vlim=None, downsample=False):
 
         self.fitsname = fitsname
 
@@ -254,43 +255,165 @@ class FitsImage:
         self.header = fits.getheader(fitsname)
 
         # read header
-        self.ndim = self.header["NAXIS"]
-        self.data_unit = self.header["BUNIT"]
+        # self.ndim = self.header["NAXIS"]
+        # self.data_unit = self.header["BUNIT"]
 
         # data
         self.data = fits.getdata(fitsname)
+        if squeeze:
+            self.data = np.squeeze(self.data)
 
-        # get axes data
-        self._get_axes_info()
+        self.rel_dir_ax = rel_dir_ax
 
-        # pixel sold angle
-        self.Omega_pix = np.abs(self.dx) * np.abs(self.dy)
+        self._get_FITS_properties(rel_dir_ax=self.rel_dir_ax)
+
+        self._cutout(xlim=xlim, ylim=ylim, vlim=vlim)
+
+        if downsample:
+            self.downsample_cube(N=downsample)
+
+    def _get_axis(self, n):
+        npix = self.header["naxis{:d}".format(n)]
+        delta = self.header["cdelt{:d}".format(n)]
+        rpix = self.header["crpix{:d}".format(n)]
+        rval = self.header["crval{:d}".format(n)]
+
+        axis = delta * (np.arange(npix) - rpix + 1) + rval
+
+        return axis
+
+    def _get_directional_axis(self, relative=True):
+        x = self._get_axis(n=1)
+        y = self._get_axis(n=2)
+        if relative:
+            x -= self.header["cvral1"]
+            y -= self.header["crval2"]
+        x *= deg_to_arcsec
+        y *= deg_to_arcsec
+        return x, y
+
+    def _get_spectral_axis(self):
+        n = self._find_spectral_axis_in_header()
+        if "freq" in self.header["ctype{:d}".format(n)].lower():
+            nu = self._get_axis(n=n)
+            v = (self.nu0 - nu) * ckms / self.nu0 
+            return nu, v
+        else:
+            v = self._get_axis(n=n)
+            vunit = self._get_axis_unit_in_header(n=n)
+            if not "k" in vunit: # the case for in m/s
+                v *= 1e3 # in km/s
+            nu = (1 - v / ckms) * self.nu0
+        return nu, v
+
+    def _get_axis_unit_in_header(self, n):
+        return self.header["cunit{:d}".format(n)]
+        
+    def _find_spectral_axis_in_header(self):
+        for n in range(1, self.header["naxis"]+1):
+            if "freq" in self.header["ctype{:d}".format(n)].lower():
+                return n 
+            if "vel" in self.header["ctype{:d}".format(n)].lower():
+                return n
+
+    def _get_restfreq(self):
+        try:
+            return self.header["restfreq"]
+        except KeyError:
+            return None
+
+    def _get_FITS_properties(self, rel_dir_ax=True):
+
+        # data unit
+        self.data_unit = self.header["bunit"]
+
+        # directional axis
+        self.x, self.y = self._get_directional_axis(relative=rel_dir_ax)
+
+        # spectral axis
+        self.nu0 = self._get_restfreq()
+        if self.ndim > 2:
+            self.nu, self.v = self._get_spectral_axis()
 
         # beam
         self._get_beam_info()
 
-        # frequency
-        if "RESTFRQ" in self.header:
-            self.restfreq = self.header["RESTFRQ"]
+    def shift_phasecenter(self, dx=0.0, dy=0.0):
+        self.x -= dx
+        self.y -= dy
 
-    def _get_axes_info(self):
-        ### standard image (2D or 3D) should have two directinal (and one spectral) axes;
-        ### or, non-standard (e.g., PV diagram) should have one directinal and one spectral axes.
-        ### This will be dealt with by "CTYPEi" argument in the header
+    def shift_phasecenter_toward(self, coord):
+        c = SkyCoord(coord, frame="icrs")
+        if self.rel_dir_ax:
+            dx = c.ra.arcsec - self.header["crval1"]
+            dy = c.dec.arcsec - self.header["crval2"]
+        else:
+            x0, y0 = self.get_phasecenter_coord()
+            dx = c.ra.arcsec - x0
+            dy = c.dec.arcsec - y0
+        self.x -= dx
+        self.y -= dy
 
-        if self.ndim == 4:
-            axes = ["x", "y", "z", "w"]  # ra, dec, spectral, stokes
-        elif self.ndim == 3:
-            axes = ["x", "y", "z"]  # ra, dec, spectral; or directinal, spectral, stokes
-        elif self.ndim == 2:
-            axes = ["x", "y"]  # ra, dec; or directinal, spectral
-        for i, ax in enumerate(axes):
-            setattr(
-                self, "n" + ax, self.header["NAXIS{:d}".format(i + 1)]
-            )  # header axis numbered from 1
-            setattr(self, "d" + ax, self.header["CDELT{:d}".format(i + 1)])
-            setattr(self, ax + "0", self.header["CRVAL{:d}".format(i + 1)])
-            setattr(self, "u" + ax, self.header["CUNIT{:d}".format(i + 1)])
+    def get_phasecenter_coord(self):
+        x, y = self._get_directional_axis(relative=False)
+        ret = []
+        for c in [x, y]:
+            q, mod = divmod(c.size, 2)
+            if mod == 0:
+                ret.append((c[q-1] + c[q]) / 2)
+            else:
+                ret.append(c[q])
+        return ret
+
+    def _cutout(self, xlim=None, ylim=None, vlim=None):
+        if xlim is not None:
+            self.data = self.data[..., is_within(self.x, xlim)]
+            self.x = self.x[is_within(self.x, xlim)]
+        if ylim is not None:
+            self.data = self.data[..., is_within(self.y, ylim), :]
+            self.y = self.y[is_within(self.y, ylim)]
+        if vlim is not None:
+            assert self.ndim > 2
+            self.data = self.data[is_within(self.v, vlim), :, :]
+            self.v = self.v[is_within(self.v, vlim)]
+
+    def downsample_cube(self, N):
+        # adopted from eddy code by rich teague
+        N = int(np.ceil(self.bmaj / (abs(self.dx)*deg_to_arcsec))) if N == 'beam' else N
+        if N > 1:
+            self.x = self.x[::N]
+            self.y = self.y[::N]
+            self.data = self.data[..., ::N, ::N]
+
+    def restore_original_cube(self, rel_dir_ax=True, squeeze=True):
+        self.data = fits.getdata(self.fitsname)
+        if squeeze:
+            self.data = np.squeeze(self.data)
+
+        self.rel_dir_ax = rel_dir_ax
+        self._get_FITS_properties(rel_dir_ax=rel_dir_ax)
+
+    def _get_cart_sky_coord(self, x0, y0):
+        return np.meshgrid(self.x - x0, self.y - y0)
+
+    def _get_cart_disk_coord(self, x0, y0, PA, incl):
+        x_sky, y_sky = self._get_cart_sky_coord(x0, y0)
+        x_rot, y_rot = self._rotate_coord(x_sky, y_sky, PA)
+        return self._deproject_coord(x_rot, y_rot, incl)
+
+    def _get_polar_disk_coord(self, x0, y0, PA, incl):
+        x_cart, y_cart = self._get_cart_disk_coord(x0, y0, PA, incl)
+        return np.hypot(x_cart, y_cart), np.arctan2(y_cart, x_cart)
+
+    @staticmethod
+    def _rotate_coord(x, y, PA):
+        x_rot = x * np.sin(np.radians(PA)) + y * np.cos(np.radians(PA))
+        y_rot = x * np.cos(np.radians(PA)) - y * np.sin(np.radians(PA))
+        return x_rot, y_rot
+
+    @staticmethod
+    def _deproject_coord(x, y, incl):
+        return x, y / np.cos(np.radians(incl))
 
     def _get_beam_info(self):
         """Fetch the beam information in header.
@@ -307,131 +430,160 @@ class FitsImage:
         self.bpa = self.header["BPA"]
 
         ### beam solid angle
-        self.Omega_beam = np.pi / (4 * np.log(2)) * self.bmaj * self.bmin
+        self.Omega_beam_arcsec2 = np.pi / (4 * np.log(2)) * self.bmaj * self.bmin
+        self.Omega_beam_str = self.Omega_beam_arcsec2 * arcsec_to_rad**2
 
         ### tuple of bmaj, bmin, and bpa
-        self.beam = (self.bmaj, self.bmin, self.bpa)
+        # self.beam = (self.bmaj, self.bmin, self.bpa)
 
         # return self.beam # to make accesible from outside
 
-    def get_directional_coord(self, center_coord=None, in_arcsec=True):
-        """Generate a (RA\cos(Dec), Dec) coordinate (1D each) in arcsec. Assume the unit for coordinates in the header is deg.
+    @property
+    def ndim(self):
+        return self.data.ndim
 
-        Args:
-            header (dict): FITS header.
-            center_coord (tuple or astropy.coordinates.SkyCoord object, optinal): Two component tuple of (RA, Dec) in arcsec or the SkyCoord object for the center coordinate. Defaults to (0.0, 0.0)
+    @property
+    def shape(self):
+        return self.data.shape
 
-        Returns:
-            tuple: Coordinates
-        """
+    @property
+    def beam(self):
+        return self.bmaj, self.bmin, self.bpa
 
-        x0 = self.x0
-        y0 = self.y0
-        dx = self.dx
-        dy = self.dy
+    @property
+    def restfreq(self):
+        return self.nu0
 
-        if in_arcsec:
-            assert self.ux == self.uy == "deg"
-            x0 *= deg_to_arcsec
-            y0 *= deg_to_arcsec
-            dx *= deg_to_arcsec
-            dy *= deg_to_arcsec
-        if center_coord is None:
-            offset_x, offset_y = 0, 0
-        else:
-            if isinstance(center_coord, tuple):
-                center_x, center_y = center_coord
-            elif isinstance(center_coord, SkyCoord):
-                center_x = center_coord.ra.arcsec
-                center_y = center_coord.dec.arcsec
-            elif isinstance(center_coord, str):
-                center_coord = SkyCoord(center_coord, frame="icrs")
-                center_x = center_coord.ra.arcsec
-                center_y = center_coord.dec.arcsec
-            offset_x = center_x - x0  # offset along x from phsecenter in arcsec
-            offset_y = center_y - y0  # offset along y from phsecenter in arcsec
+    @property
+    def nu0(self):
+        return self.nu0
 
-        self.x = dx * (np.arange(self.nx) - (self.header["CRPIX1"] - 1)) - offset_x
-        self.y = dy * (np.arange(self.ny) - (self.header["CRPIX2"] - 1)) - offset_y
+    @property
+    def nxpix(self):
+        return self.x.size
 
-    def get_projected_coord(self, PA=90.0, incl=0.0, center_coord=None, in_arcsec=True):
-        self.get_directional_coord(center_coord=center_coord, in_arcsec=in_arcsec)
+    @property
+    def nypix(self):
+        return self.y.size
 
-        # meshgrid to be in 2D
-        xx, yy = np.meshgrid(self.x, self.y)
+    # def get_directional_coord(self, center_coord=None, in_arcsec=True):
+    #     """Generate a (RA\cos(Dec), Dec) coordinate (1D each) in arcsec. Assume the unit for coordinates in the header is deg.
 
-        # project to the disk plane; assume geometrically thin disk
-        incl = np.radians(incl)
-        PA = np.radians(PA)
+    #     Args:
+    #         header (dict): FITS header.
+    #         center_coord (tuple or astropy.coordinates.SkyCoord object, optinal): Two component tuple of (RA, Dec) in arcsec or the SkyCoord object for the center coordinate. Defaults to (0.0, 0.0)
 
-        self.x_proj = xx * np.sin(PA) + yy * np.cos(PA)
-        self.y_proj = (-xx * np.cos(PA) + yy * np.sin(PA)) / np.cos(
-            incl
-        )  # follow the formulation in Yen et al. 2016
+    #     Returns:
+    #         tuple: Coordinates
+    #     """
 
-        # polar coordinate
-        self.r = np.sqrt(self.x_proj**2 + self.y_proj**2)  # in arcsec
-        self.theta = np.degrees(
-            np.arctan2(self.y_proj, self.x_proj)
-        )  # in degree, [-180, 180]
+    #     x0 = self.x0
+    #     y0 = self.y0
+    #     dx = self.dx
+    #     dy = self.dy
 
-    def get_spectral_coord(self):
-        if self.ndim < 3:
-            raise KeyError("Spectral axis not found.")
+    #     if in_arcsec:
+    #         assert self.ux == self.uy == "deg"
+    #         x0 *= deg_to_arcsec
+    #         y0 *= deg_to_arcsec
+    #         dx *= deg_to_arcsec
+    #         dy *= deg_to_arcsec
+    #     if center_coord is None:
+    #         offset_x, offset_y = 0, 0
+    #     else:
+    #         if isinstance(center_coord, tuple):
+    #             center_x, center_y = center_coord
+    #         elif isinstance(center_coord, SkyCoord):
+    #             center_x = center_coord.ra.arcsec
+    #             center_y = center_coord.dec.arcsec
+    #         elif isinstance(center_coord, str):
+    #             center_coord = SkyCoord(center_coord, frame="icrs")
+    #             center_x = center_coord.ra.arcsec
+    #             center_y = center_coord.dec.arcsec
+    #         offset_x = center_x - x0  # offset along x from phsecenter in arcsec
+    #         offset_y = center_y - y0  # offset along y from phsecenter in arcsec
 
-        # assume in frequency
-        self.nu = self.dz * (np.arange(self.nz) - (self.header["CRPIX3"] - 1)) + self.z0
+    #     self.x = dx * (np.arange(self.nx) - (self.header["CRPIX1"] - 1)) - offset_x
+    #     self.y = dy * (np.arange(self.ny) - (self.header["CRPIX2"] - 1)) - offset_y
 
-        assert self.header["VELREF"] == 257  # in radio convention
-        self.v = ckms * (1 - self.nu / self.restfreq)
+    # def get_projected_coord(self, PA=90.0, incl=0.0, center_coord=None, in_arcsec=True):
+    #     self.get_directional_coord(center_coord=center_coord, in_arcsec=in_arcsec)
+
+    #     # meshgrid to be in 2D
+    #     xx, yy = np.meshgrid(self.x, self.y)
+
+    #     # project to the disk plane; assume geometrically thin disk
+    #     incl = np.radians(incl)
+    #     PA = np.radians(PA)
+
+    #     self.x_proj = xx * np.sin(PA) + yy * np.cos(PA)
+    #     self.y_proj = (-xx * np.cos(PA) + yy * np.sin(PA)) / np.cos(
+    #         incl
+    #     )  # follow the formulation in Yen et al. 2016
+
+    #     # polar coordinate
+    #     self.r = np.sqrt(self.x_proj**2 + self.y_proj**2)  # in arcsec
+    #     self.theta = np.degrees(
+    #         np.arctan2(self.y_proj, self.x_proj)
+    #     )  # in degree, [-180, 180]
+
+    # def get_spectral_coord(self):
+    #     if self.ndim < 3:
+    #         raise KeyError("Spectral axis not found.")
+
+    #     # assume in frequency
+    #     self.nu = self.dz * (np.arange(self.nz) - (self.header["CRPIX3"] - 1)) + self.z0
+
+    #     assert self.header["VELREF"] == 257  # in radio convention
+    #     self.v = ckms * (1 - self.nu / self.restfreq)
 
     
-    def cutout(self, xlim=None, ylim=None, vlim=None):
-        if xlim is not None:
-            self.data = self.data[..., is_within(self.x, xlim)]
-            try:
-                self.x_proj = self.x_proj[:, is_within(self.x, xlim)]
-                self.y_proj = self.y_proj[:, is_within(self.x, xlim)]
-                self.r = self.r[:, is_within(self.x, xlim)]
-                self.theta = self.theta[:, is_within(self.x, xlim)]
-            except AttributeError:
-                pass
-            self.x = self.x[is_within(self.x, xlim)]
+    # def cutout(self, xlim=None, ylim=None, vlim=None):
+    #     if xlim is not None:
+    #         self.data = self.data[..., is_within(self.x, xlim)]
+    #         try:
+    #             self.x_proj = self.x_proj[:, is_within(self.x, xlim)]
+    #             self.y_proj = self.y_proj[:, is_within(self.x, xlim)]
+    #             self.r = self.r[:, is_within(self.x, xlim)]
+    #             self.theta = self.theta[:, is_within(self.x, xlim)]
+    #         except AttributeError:
+    #             pass
+    #         self.x = self.x[is_within(self.x, xlim)]
 
-        if ylim is not None:
-            self.data = self.data[..., is_within(self.y, ylim), :]
-            try:
-                self.x_proj = self.x_proj[is_within(self.y, ylim), :]
-                self.y_proj = self.y_proj[is_within(self.y, ylim), :]
-                self.r = self.r[is_within(self.y, ylim), :]
-                self.theta = self.theta[is_within(self.y, ylim), :]
-            except AttributeError:
-                pass
-            self.y = self.y[is_within(self.y, ylim)]
+    #     if ylim is not None:
+    #         self.data = self.data[..., is_within(self.y, ylim), :]
+    #         try:
+    #             self.x_proj = self.x_proj[is_within(self.y, ylim), :]
+    #             self.y_proj = self.y_proj[is_within(self.y, ylim), :]
+    #             self.r = self.r[is_within(self.y, ylim), :]
+    #             self.theta = self.theta[is_within(self.y, ylim), :]
+    #         except AttributeError:
+    #             pass
+    #         self.y = self.y[is_within(self.y, ylim)]
 
-        if vlim is not None:
-            self.data = self.data[is_within(self.v, vlim)]
-            self.nu = self.nu[is_within(self.v, vlim)]
-            self.v = self.v[is_within(self.v, vlim)]
+    #     if vlim is not None:
+    #         self.data = self.data[is_within(self.v, vlim)]
+    #         self.nu = self.nu[is_within(self.v, vlim)]
+    #         self.v = self.v[is_within(self.v, vlim)]
         
-    def downsample(self, N):
-        # adopted from eddy code by rich teague
-        N = int(np.ceil(self.bmaj / (abs(self.dx)*deg_to_arcsec))) if N == 'beam' else N
-        N0x, N0y = int(N / 2), int(N / 2)
-        if N > 1:
-            self.x = self.x[N0x::N]
-            self.y = self.y[N0y::N]
-            self.data = self.data[..., N0y::N, N0x::N]
-            try:
-                self.x_proj = self.x_proj[N0y::N, N0x::N]
-                self.y_proj = self.y_proj[N0y::N, N0x::N]
-                self.r = self.r[N0y::N, N0x::N]
-                self.theta = self.theta[N0y::N, N0x::N]
-                # self.mask = self.mask[N0y::N, N0x::N]
-            except AttributeError:
-                pass
-            # self.error = self.error[N0y::N, N0x::N]
-            # self.mask = self.mask[N0y::N, N0x::N]
+    # def downsample(self, N):
+    #     # adopted from eddy code by rich teague
+    #     N = int(np.ceil(self.bmaj / (abs(self.dx)*deg_to_arcsec))) if N == 'beam' else N
+    #     N0x, N0y = int(N / 2), int(N / 2)
+    #     if N > 1:
+    #         self.x = self.x[N0x::N]
+    #         self.y = self.y[N0y::N]
+    #         self.data = self.data[..., N0y::N, N0x::N]
+    #         try:
+    #             self.x_proj = self.x_proj[N0y::N, N0x::N]
+    #             self.y_proj = self.y_proj[N0y::N, N0x::N]
+    #             self.r = self.r[N0y::N, N0x::N]
+    #             self.theta = self.theta[N0y::N, N0x::N]
+    #             # self.mask = self.mask[N0y::N, N0x::N]
+    #         except AttributeError:
+    #             pass
+    #         # self.error = self.error[N0y::N, N0x::N]
+    #         # self.mask = self.mask[N0y::N, N0x::N]
 
     def get_threshold_mask(self, threshold=3):
         self.SNR = self.data / self.rms 
