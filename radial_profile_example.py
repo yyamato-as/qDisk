56f382a1423de540f49bb0c0c1f2522c0d6f40fb
yyamato-as <yyamato.as@gmail.com>
2022-08-23 17:44:32 +0900

    Updates

--- radial_profile_example.py
+++ radial_profile_example.py
@@ -1,5 +1,7 @@
 from qdisk.classes import FitsImage
+from qdisk.plot import Map
 from eDisk_source_dict import source_dict
+from astropy.visualization import AsinhStretch
 import matplotlib.pyplot as plt
 
 source = "L1489IRS"
@@ -21,6 +23,15 @@ fig, ax = plt.subplots(constrained_layout=True)
 ax.plot(r, I)
 ax.fill_between(r, I-dI, I+dI, alpha=0.25)
 ax.set(xlim=(0.05, rmax), ylim=(0.02e-3, 20e-3), xlabel="Radius [arcsec]", ylabel="$I$ [Jy / beam]", xscale="log", yscale="log")
+
+plt.show()
+
+# check the average region
+fig, ax = plt.subplots(constrained_layout=True)
+imagemap = Map(imagename, ax=ax, center_coord=center_coord, xlim=(-rmax, rmax), ylim=(-rmax, rmax))
+imagemap.plot_colormap(cmap="inferno", vmin=0.0, stretch=AsinhStretch(a=0.02))
+mask = imagemap.get_mask(rmax=rmax, thetamin=wedge_angle, thetamax=180 - wedge_angle, abs_theta=True, exclude_theta=True, PA=source_dict[source]["PA"], incl=source_dict[source]["incl"])
+imagemap.overlay_contour(mask, x=imagemap.x, y=imagemap.y)
 
 plt.show()
 
